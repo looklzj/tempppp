61bfbfaba32cc72333667b2c88ca1b1628832de5
lookNa2CO3 <lookinward.qq@gmail.com>
2020-05-23 18:56:51 +0800

    x

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,20 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/',
+    path: '/project_input',
     name: 'xiang_mu_lu_ru',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/project_input.vue')
+  },
+  {
+    path: '/',
+    name: 'xiang_mu_lu_ru',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/scenic_input.vue')
   }
 ]
 
