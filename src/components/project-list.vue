6c436e874069b8c8b5caf36ec78162aa26702376
lookNa2CO3 <lookinward.qq@gmail.com>
2020-07-04 19:47:43 +0800

    x

--- src/components/project-list.vue
+++ src/components/project-list.vue
@@ -10,6 +10,9 @@
         label="客户名称"
         width="180"
       >
+        <template slot-scope="scope">
+          <router-link :to="'project-input?id='+scope.row.id">{{scope.row.customer_name}}</router-link>
+        </template>
       </el-table-column>
       <el-table-column
         prop="customer_phone"
@@ -31,7 +34,6 @@
         :filter-method="filterTag"
         filter-placement="bottom-end"
       >
-
       </el-table-column>
       <el-table-column
         prop="company_name"
@@ -45,25 +47,88 @@
         width="180"
       >
       </el-table-column>
+
       <el-table-column
-        prop="address"
-        label="地址"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="tag"
-        label="标签"
+        prop="customer_type"
+        label="客户类别"
         width="100"
-        :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+        :filters="customer_types"
         :filter-method="filterTag"
         filter-placement="bottom-end"
       >
       </el-table-column>
+
       <el-table-column
-        prop="company_name"
-        label="公司名称"
+        prop="project_type"
+        label="项目类型"
+        width="100"
+        :filters="project_types"
+        :filter-method="filterTag"
+        filter-placement="bottom-end"
       >
       </el-table-column>
+
+      <el-table-column
+        prop="need_product"
+        label="需求产品"
+        width="180"
+      >
+      </el-table-column>
+
+      <el-table-column
+        prop="project_addr"
+        label="项目地址"
+        width="180"
+      >
+      </el-table-column>
+
+      <el-table-column
+        prop="project_name"
+        label="项目名称"
+        width="180"
+      >
+      </el-table-column>
+
+      <el-table-column
+        prop="project_state"
+        label="项目状况"
+        width="100"
+        :filters="project_states"
+        :filter-method="filterTag"
+        filter-placement="bottom-end"
+      >
+      </el-table-column>
+
+      <el-table-column
+        prop="customer_level"
+        label="客户等级"
+        width="100"
+        :filters="customer_levels"
+        :filter-method="filterTag"
+        filter-placement="bottom-end"
+      >
+      </el-table-column>
+
+      <el-table-column
+        prop="init_comment"
+        width="300"
+        label="初次沟通内容"
+      >
+      </el-table-column>
+
+      <el-table-column
+        prop="username"
+        label="提交者"
+      >
+      </el-table-column>
+
+      <el-table-column
+        prop="created_at"
+        width="110"
+        label="时间"
+      >
+      </el-table-column>
+
     </el-table>
   </div>
 </template>
@@ -99,6 +164,37 @@ export default {
         { text: "客户推荐", value: "客户推荐" },
         { text: "渠道代理", value: "渠道代理" },
         { text: "自行开发", value: "自行开发" }
+      ],
+      customer_types: [
+        { text: "甲方单位", value: "甲方单位" },
+        { text: "总包", value: "总包" },
+        { text: "分包", value: "分包" },
+        { text: "设计公司", value: "设计公司" },
+        { text: "投资方", value: "投资方" },
+        { text: "策划公司", value: "策划公司" },
+        { text: "中间人", value: "中间人" },
+        { text: "运营公司", value: "运营公司" }
+      ],
+      project_types: [
+        { text: "文化旅游", value: "文化旅游" },
+        { text: "视觉工程", value: "视觉工程" },
+        { text: "数字展馆", value: "数字展馆" },
+        { text: "舞美娱乐", value: "舞美娱乐" },
+        { text: "其它", value: "其它" }
+      ],
+      project_states: [
+        { text: "项目选址", value: "项目选址" },
+        { text: "提案阶段", value: "提案阶段" },
+        { text: "项目报批", value: "项目报批" },
+        { text: "造价报价", value: "造价报价" },
+        { text: "合同审核", value: "合同审核" },
+        { text: "签约履约", value: "签约履约" }
+      ],
+      customer_levels: [
+        { text: "A", value: "A" },
+        { text: "B", value: "B" },
+        { text: "C", value: "C" },
+        { text: "D", value: "D" }
       ]
     };
   },
@@ -112,7 +208,6 @@ export default {
   },
   methods: {
     filterTag(value, row) {
-      console.log(value);
       console.log(row);
       return row.customer_origin === value;
     },
@@ -171,6 +266,16 @@ export default {
         }
       }
     }
+  }
+  /deep/ .el-table .cell {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  /deep/ .el-table a {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color: #606266;
+    text-decoration: none;
   }
 }
 </style>
